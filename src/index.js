9b0d8d69849489455a4aa406966fc393c3c32425
unknown <sholomitkyR@yandex.by>
2021-08-17 11:30:50 +0300

    feat: create reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let rev = String(Math.abs(n))
+    let reversed = ''
+    for (let i = 0; i < rev.length; i++)
+        {reversed = rev[i] + reversed }
+        return Number(reversed)
 }
+
+ 
